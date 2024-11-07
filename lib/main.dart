2a7bd058ac827ce4899954d956770f0bdc03288b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-07 18:06:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/spotify_app.dart';
+
+void main() {
+  runApp(const SpotifyApp());
+}
+
+
---
+++ lib/spotify_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/theme/spotify_app_theme.dart';
+import 'package:spotify/features/on_boarding/get_started/presentation/views/get_started_view.dart';
+
+class SpotifyApp extends StatelessWidget {
+  const SpotifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: SpotifyAppTheme.lightTheme,
+      darkTheme: SpotifyAppTheme.darkTheme,
+      home: GetStartedView(),
+    );
+  }
+}
