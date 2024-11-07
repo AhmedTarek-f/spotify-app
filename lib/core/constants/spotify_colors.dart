9e5201aaefd75c66c931f0fcc80be64c88f91131
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-07 18:10:18 +0200

    adding spotify app colors

---
+++ lib/core/constants/spotify_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class SpotifyColors{
+  static const Color primaryColor = Color(0xff42C83C);
+  static const Color dark = Color (0xFF272727);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color (0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+}
