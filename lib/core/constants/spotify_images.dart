a6800cbb97512b687840d3c38b92d0c6a93d452f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-07 18:47:15 +0200

    adding spotify images

---
+++ lib/core/constants/spotify_images.dart
@@ -0,0 +1,28 @@
+abstract class SpotifyImages {
+  static const String appleIcon= "assets/images/icons/apple_icon.svg";
+  static const String discoveryIcon= "assets/images/icons/Discovery.svg";
+  static const String googleIcon= "assets/images/icons/google_icon.svg";
+  static const String heartIcon= "assets/images/icons/Heart.svg";
+  static const String homeIcon= "assets/images/icons/Home.svg";
+  static const String leftArrowIcon= "assets/images/icons/left_arrow.svg";
+  static const String loveIcon= "assets/images/icons/love_icon.svg";
+  static const String moonIcon= "assets/images/icons/Moon.svg";
+  static const String playIcon= "assets/images/icons/Play.svg";
+  static const String profileIcon= "assets/images/icons/Profile.svg";
+  static const String sunIcon= "assets/images/icons/Sun.svg";
+  static const String spotifyLogoPNG= "assets/images/logos/spotify_logo.png";
+  static const String spotifyLogoSVG= "assets/images/logos/spotify_logo.svg";
+  static const String onBoarding1= "assets/images/on_boarding/on_boarding_1.svg";
+  static const String onBoarding2= "assets/images/on_boarding/on_boarding_2.svg";
+  static const String onBoarding3= "assets/images/on_boarding/on_boarding_3.svg";
+  static const String onBoarding3BottomSlashes= "assets/images/on_boarding/on_boarding_3_bottom_slashes.svg";
+  static const String onBoarding3TopSlashes= "assets/images/on_boarding/on_boarding_3_top_slashes.svg";
+  static const String album1= "assets/images/songs_images/album_1.svg";
+  static const String album_2= "assets/images/songs_images/album_2.svg";
+  static const String articlePageUpperPart= "assets/images/songs_images/artics_page_upper_part.svg";
+  static const String homeAlbum1= "assets/images/songs_images/home_album_1.svg";
+  static const String lyricsBackgroundImage= "assets/images/songs_images/lyrics_bacgr.svg";
+  static const String song1= "assets/images/songs_images/song1.svg";
+  static const String song2= "assets/images/songs_images/song2.svg";
+  static const String song3= "assets/images/songs_images/song3.svg";
+}
