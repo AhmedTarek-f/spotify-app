728c25fd9d08521e7b45a2398b0007407014b115
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-07 18:10:42 +0200

    adding spotify app fonts

---
+++ lib/core/constants/spotify_fonts.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+abstract class SpotifyFonts{
+ static const TextStyle appStylesBold25 = TextStyle(
+   fontSize: 25,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesRegular17 = TextStyle(
+   fontSize: 17,
+   fontWeight: FontWeight.w400,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold22 = TextStyle(
+   fontSize: 22,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium17 = TextStyle(
+   fontSize: 17,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold26 = TextStyle(
+   fontSize: 26,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium19 = TextStyle(
+   fontSize: 19,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold30 = TextStyle(
+   fontSize: 30,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesRegular12 = TextStyle(
+   fontSize: 12,
+   fontWeight: FontWeight.w400,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium16 = TextStyle(
+   fontSize: 16,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium14 = TextStyle(
+   fontSize: 14,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium10 = TextStyle(
+   fontSize: 10,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold20 = TextStyle(
+   fontSize: 20,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold19 = TextStyle(
+   fontSize: 19,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium13 = TextStyle(
+   fontSize: 13,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium20 = TextStyle(
+   fontSize: 20,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold16 = TextStyle(
+   fontSize: 16,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesRegular14 = TextStyle(
+   fontSize: 14,
+   fontWeight: FontWeight.w400,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesRegular15 = TextStyle(
+   fontSize: 15,
+   fontWeight: FontWeight.w400,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold18 = TextStyle(
+   fontSize: 18,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesRegular20 = TextStyle(
+   fontSize: 20,
+   fontWeight: FontWeight.w400,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesMedium12 = TextStyle(
+   fontSize: 12,
+   fontWeight: FontWeight.w500,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold15 = TextStyle(
+   fontSize: 15,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesRegular13 = TextStyle(
+   fontSize: 13,
+   fontWeight: FontWeight.w400,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold13 = TextStyle(
+   fontSize: 13,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+ static const TextStyle appStylesBold17 = TextStyle(
+   fontSize: 17,
+   fontWeight: FontWeight.w700,
+   overflow: TextOverflow.ellipsis,
+ );
+}
