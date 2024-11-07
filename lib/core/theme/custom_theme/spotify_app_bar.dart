e30482b6140eb9524be0104a71e871e811032fa9
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-07 18:11:48 +0200

    adding spotify custom themes

---
+++ lib/core/theme/custom_theme/spotify_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class SpotifyAppBar
+{
+  static const AppBarTheme lightAppBarTheme = AppBarTheme(
+      elevation: 0,
+      centerTitle: false,
+      scrolledUnderElevation: 0,
+      backgroundColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+  );
+  static const AppBarTheme darkAppBarTheme = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors. transparent,
+    surfaceTintColor: Colors. transparent,
+  );
+}
