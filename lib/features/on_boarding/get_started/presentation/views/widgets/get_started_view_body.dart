14691bf904512c0ab25aa2a23f7230ef8faba521
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-08 13:54:01 +0200

    adding get started view design

---
+++ lib/features/on_boarding/get_started/presentation/views/widgets/get_started_view_body.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/common_widgets/spotify_custom_button.dart';
+import 'package:spotify/core/constants/spotify_fonts.dart';
+import 'package:spotify/core/constants/spotify_images.dart';
+import 'package:spotify/features/on_boarding/choose_mode/presentation/views/choose_mode_view.dart';
+
+class GetStartedViewBody extends StatelessWidget {
+  const GetStartedViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Stack(
+      children: [
+        Positioned.fill(
+          child: Image.asset(
+              SpotifyImages.onBoarding1,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Column(
+          children: [
+            SizedBox(height: MediaQuery.sizeOf(context).height*0.0438,),
+            SvgPicture.asset(SpotifyImages.spotifyLogoSVG),
+            SizedBox(height: MediaQuery.sizeOf(context).height*0.4774,),
+            Text("Enjoy Listening To Music",style: SpotifyFonts.appStylesBold25.copyWith(color: const Color(0xffDADADA)),),
+            const SizedBox(height: 25,),
+            Text(
+                "Lorem ipsum dolor sit amet,\n consectetur adipiscing elit. Sagittis enim\n purus sed phasellus. Cursus ornare id\n scelerisque aliquam.\n",
+              style: SpotifyFonts.appStylesRegular17.copyWith(color: const Color(0xff797979),overflow: TextOverflow.visible),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 37,),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: MediaQuery.sizeOf(context).width*0.0769),
+              child: SpotifyCustomButton(
+                onPressed: (){
+                  Get.off(()=> const ChooseModeView(),
+                      transition: Transition.leftToRightWithFade,
+                    duration: const Duration(milliseconds: 500)
+                  );
+                },
+                buttonTitle: "Get Started",
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
