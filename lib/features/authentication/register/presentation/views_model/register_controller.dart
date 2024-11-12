b9e1bc44beb392693626d1da46bf137a99a92810
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-12 14:41:03 +0200

    adding register controller

---
+++ lib/features/authentication/register/presentation/views_model/register_controller.dart
@@ -0,0 +1,90 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/constants/spotify_images.dart';
+import 'package:spotify/core/utlis/loaders/loaders.dart';
+import 'package:spotify/core/utlis/popups/t_full_screen_loader.dart';
+import 'package:spotify/core/utlis/services/auth_services/register_with_email_and_password_service/register_with_email_and_password_service.dart';
+import 'package:spotify/core/utlis/services/auth_services/sign_in_with_google_service/sign_in_with_google_service.dart';
+import 'package:spotify/features/authentication/data/repository/authentication_repository.dart';
+import 'package:spotify/features/authentication/register/data/models/user_model.dart';
+import 'package:spotify/features/authentication/register/data/repository/register_repository.dart';
+import 'package:spotify/features/authentication/verify_email/presentation/views/verify_email_view.dart';
+
+class RegisterController extends GetxController {
+  static RegisterController get instance => Get.find();
+
+  RxBool hidePassword = true.obs;
+  late final TextEditingController userName;
+  late final TextEditingController email;
+  late final TextEditingController password;
+  GlobalKey<FormState> registerFormKey = GlobalKey<FormState>();
+  final Rx<AutovalidateMode>  autoValidateMode = AutovalidateMode.disabled.obs;
+  final RegisterRepository _registerRepositoryController = Get.put(RegisterRepository());
+
+  @override
+  void onInit() {
+    userName = TextEditingController();
+    email = TextEditingController();
+    password = TextEditingController();
+    super.onInit();
+  }
+
+  Future<void> signUp()async {
+    try{
+      if(!registerFormKey.currentState!.validate())
+      {
+        autoValidateMode.value = AutovalidateMode.always;
+        return;
+      }
+      else{
+        registerFormKey.currentState!.save();
+        autoValidateMode.value = AutovalidateMode.disabled;
+        TFullScreenLoader.openLoadingDialog("We are processing your information...", SpotifyImages.docerAnimation);
+        final UserCredential userCredential = await RegisterWithEmailAndPasswordService.registerWithEmailAndPassword(email.text.trim(), password.text.trim());
+        final newUser = UserModel(
+          id: userCredential.user!.uid,
+          userName: userName.text.trim(),
+          email: email.text.trim(),
+        );
+
+        await _registerRepositoryController.registerUser(userData: newUser);
+        TFullScreenLoader.stopLoading();
+        Loaders.successSnackBar(title: "Congratulations",message: "Your account has been created! Verify email to continue.");
+        Get.to(()=> VerifyEmailView(email: email.text.trim(),));
+      }
+    }
+    catch(e){
+      TFullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: "Oh Snap!",message: e.toString());
+    }
+  }
+
+  Future<void> signUpWithGoogle() async
+  {
+    try{
+      TFullScreenLoader.openLoadingDialog("We are processing your information...", SpotifyImages.docerAnimation);
+      final UserCredential userCredential = await SignInWithGoogleService.signInWithGoogle();
+      if(userCredential.additionalUserInfo?.isNewUser ?? false){
+        await _registerRepositoryController.saveGoogleUserRecord(userCredential);
+      }
+      TFullScreenLoader.stopLoading();
+      await AuthenticationRepository.instance.screenRedirect();
+      Get.offAll(()=>AuthenticationRepository.instance.redirectedScreen);
+    }
+    catch(e)
+    {
+      TFullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: "Data not saved!",message: e.toString());
+    }
+  }
+
+
+  @override
+  void onClose() {
+    userName.dispose();
+    email.dispose();
+    password.dispose();
+    super.onClose();
+  }
+}
