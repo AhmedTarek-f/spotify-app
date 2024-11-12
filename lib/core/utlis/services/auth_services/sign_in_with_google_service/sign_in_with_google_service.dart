25e393f22049147570d1306817f6cb2148609661
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-12 14:34:07 +0200

    adding sign in with google service

---
+++ lib/core/utlis/services/auth_services/sign_in_with_google_service/sign_in_with_google_service.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:spotify/core/utlis/exceptions/t_firebase_auth_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_firebase_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_format_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_platform_exceptions.dart';
+
+abstract class SignInWithGoogleService {
+  static final _auth = FirebaseAuth.instance;
+  static Future<UserCredential> signInWithGoogle() async
+  {
+    try{
+      final GoogleSignInAccount? userAccount = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth = await userAccount?.authentication;
+
+      final  credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      return await _auth.signInWithCredential(credential);
+    }
+    on FirebaseAuthException catch (e)
+    {
+      throw TFirebaseAuthException(e.code).message;
+    }
+    on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on FormatException catch (_){
+      throw const TFormatException();
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again";
+    }
+  }
+}
