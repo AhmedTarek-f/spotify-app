a7f2587584382570d8348405ab3a620431d8e141
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-12 14:41:54 +0200

    adding sign in controller

---
+++ lib/features/authentication/sign_in/presentation/views_model/sign_in_controller.dart
@@ -0,0 +1,81 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/constants/spotify_images.dart';
+import 'package:spotify/core/utlis/loaders/loaders.dart';
+import 'package:spotify/core/utlis/popups/t_full_screen_loader.dart';
+import 'package:spotify/core/utlis/services/auth_services/sign_in_with_google_service/sign_in_with_google_service.dart';
+import 'package:spotify/features/authentication/data/repository/authentication_repository.dart';
+import 'package:spotify/features/authentication/sign_in/data/repository/sign_in_repository.dart';
+
+class SignInController extends GetxController {
+  static SignInController get instance => Get.find();
+  late final TextEditingController email;
+  late final TextEditingController password;
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  final Rx<AutovalidateMode> autoValidateMode = AutovalidateMode.disabled.obs;
+  final SignInRepository signInRepository = Get.put(SignInRepository());
+  RxBool hidePassword = true.obs;
+
+
+  @override
+  void onInit() {
+    super.onInit();
+    email = TextEditingController();
+    password = TextEditingController();
+  }
+
+  Future<void> signIn() async{
+    try{
+      if(!loginFormKey.currentState!.validate())
+      {
+        autoValidateMode.value = AutovalidateMode.always;
+        return;
+      }
+      else{
+        loginFormKey.currentState!.save();
+        autoValidateMode.value = AutovalidateMode.disabled;
+        TFullScreenLoader.openLoadingDialog("Logging you in...".tr, SpotifyImages.docerAnimation);
+        await signInRepository.signInWithEmailAndPassword(email.text.trim(), password.text.trim());
+        clearData();
+        await AuthenticationRepository.instance.screenRedirect();
+        TFullScreenLoader.stopLoading();
+        Get.offAll(() => AuthenticationRepository.instance.redirectedScreen);
+      }
+    }
+    catch(e){
+      TFullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: "Oh Snap!".tr,message: e.toString().tr);
+    }
+  }
+
+  Future<void> googleSignIn()async {
+    try {
+      TFullScreenLoader.openLoadingDialog(
+          "Logging you in...".tr, SpotifyImages.docerAnimation);
+      final UserCredential userCredential = await SignInWithGoogleService.signInWithGoogle();
+      if (userCredential.additionalUserInfo?.isNewUser ?? false) {
+        await signInRepository.saveGoogleUserRecord(userCredential);
+      }
+      await AuthenticationRepository.instance.screenRedirect();
+      TFullScreenLoader.stopLoading();
+      Get.offAll(() => AuthenticationRepository.instance.redirectedScreen);
+    }
+    catch (e) {
+      TFullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: "Oh Snap!".tr, message: e.toString().tr);
+    }
+  }
+
+  void clearData() {
+    email.clear();
+    password.clear();
+  }
+
+  @override
+  void onClose() {
+    email.dispose();
+    password.dispose();
+    super.onClose();
+  }
+}
