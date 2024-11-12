59dec02bdf12b87c9172573fd2e7821b152e6003
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-12 14:36:02 +0200

    adding auth remote data sources

---
+++ lib/features/authentication/data/data_sources/remote_data_sources/auth_remote_data.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:spotify/core/utlis/exceptions/t_firebase_auth_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_firebase_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_format_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_platform_exceptions.dart';
+import 'package:spotify/features/authentication/register/data/models/user_model.dart';
+import 'package:spotify/features/authentication/sign_in/presentation/views/sign_in_view.dart';
+
+class AuthRemoteData extends GetxController{
+  static AuthRemoteData get instance => Get.find();
+  final _db = FirebaseFirestore.instance;
+
+  // LogOut User
+  Future<void> logout() async
+  {
+    try{
+      await GoogleSignIn().signOut();
+      await FirebaseAuth.instance.signOut();
+      Get.offAll(()=> const SignInView());
+    }on FirebaseAuthException catch (e)
+    {
+      throw TFirebaseAuthException(e.code).message;
+    }
+    on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on FormatException catch (_){
+      throw const TFormatException();
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again";
+    }
+
+  }
+
+  Future<void> deleteAccount(String userId) async{
+    try{
+      await _db.collection("Users").doc(userId).delete();
+      await FirebaseAuth.instance.currentUser!.delete();
+    }on FirebaseAuthException catch (e)
+    {
+      throw TFirebaseAuthException(e.code).message;
+    }
+    on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on FormatException catch (_){
+      throw const TFormatException();
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again";
+    }
+  }
+
+}
