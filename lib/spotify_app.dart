ce187e4765b956135a006661a76cfbdc4fa565ab
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-12 14:46:45 +0200

    using the screen redirect for home

--- lib/spotify_app.dart
+++ lib/spotify_app.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:spotify/core/theme/spotify_app_theme.dart';
-import 'package:spotify/features/on_boarding/get_started/presentation/views/get_started_view.dart';
+import 'package:spotify/features/authentication/data/repository/authentication_repository.dart';
 
 class SpotifyApp extends StatelessWidget {
   const SpotifyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final AuthenticationRepository authenticationRepository = AuthenticationRepository.instance;
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: SpotifyAppTheme.lightTheme,
       darkTheme: SpotifyAppTheme.darkTheme,
-      home: const GetStartedView(),
+      home: authenticationRepository.redirectedScreen,
     );
   }
 }
