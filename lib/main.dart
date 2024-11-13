b2efbe0435e8b2f6032fdf45b0d81a01e7ae5dda
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-13 14:21:57 +0200

    removing flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:spotify/core/constants/api_keys.dart';
 import 'package:spotify/core/utlis/functions/setup_service_locator.dart';
 import 'package:spotify/features/authentication/data/repository/authentication_repository.dart';
 import 'package:spotify/firebase_options.dart';
 
 import 'package:spotify/spotify_app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 Future<void> main() async{
-  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   setupServiceLocator();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,).then(
           (FirebaseApp value) => Get.put(AuthenticationRepository())
+  );
+  await Supabase.initialize(
+    url: projectUrl,
+    anonKey: anonKey,
   );
 
   runApp(const SpotifyApp());
