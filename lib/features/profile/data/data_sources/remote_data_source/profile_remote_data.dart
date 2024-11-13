2c223356ecda702c8a9b613e9509a8fe7dc69a1b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-13 14:20:11 +0200

    adding profile remote data

---
+++ lib/features/profile/data/data_sources/remote_data_source/profile_remote_data.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/utlis/exceptions/t_firebase_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_format_exceptions.dart';
+import 'package:spotify/core/utlis/exceptions/t_platform_exceptions.dart';
+import 'package:spotify/features/authentication/register/data/models/user_model.dart';
+
+class ProfileRemoteData extends GetxController {
+  static ProfileRemoteData get instance => Get.find();
+  final _db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  Future<UserModel> fetchUserDetails() async{
+    try{
+      final documentSnapshot = await _db.collection("Users").doc(FirebaseAuth.instance.currentUser?.uid).get();
+      if(documentSnapshot.exists)
+      {
+        return UserModel.fromSnapshot(documentSnapshot);
+      }
+      else{
+        return UserModel.empty();
+      }
+    }
+    on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on FormatException catch (_){
+      throw const TFormatException();
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again".tr;
+    }
+  }
+
+  Future<void> updateProfilePicture({required Map<String,dynamic> json}) async {
+    try{
+      await _db.collection("Users").doc(_auth.currentUser!.uid).update(json);
+    }
+    on FirebaseException catch (e){
+      throw TFirebaseException(e.code).message;
+    }
+    on FormatException catch (_){
+      throw const TFormatException();
+    }
+    on PlatformException catch(e)
+    {
+      throw TPlatformException(e.code).message;
+    }
+    catch (e)
+    {
+      throw "Something went wrong, Please try again".tr;
+    }
+  }
+
+}
