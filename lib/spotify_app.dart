61f4dba1e7d28a7c6511b48b2c7c5a4dab8a634a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-13 14:22:41 +0200

    using splash view

--- lib/spotify_app.dart
+++ lib/spotify_app.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:spotify/core/theme/spotify_app_theme.dart';
-import 'package:spotify/features/authentication/data/repository/authentication_repository.dart';
+import 'package:spotify/features/splash/presentation/views/splash_view.dart';
 
 class SpotifyApp extends StatelessWidget {
   const SpotifyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final AuthenticationRepository authenticationRepository = AuthenticationRepository.instance;
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: SpotifyAppTheme.lightTheme,
       darkTheme: SpotifyAppTheme.darkTheme,
-      home: authenticationRepository.redirectedScreen,
+      home: const SplashView(),
     );
   }
 }
