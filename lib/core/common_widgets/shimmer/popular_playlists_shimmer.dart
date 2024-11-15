b53636338393f2d7134023441b59473262295739
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-15 14:54:02 +0200

    adding popular playlists shimmer widget

---
+++ lib/core/common_widgets/shimmer/popular_playlists_shimmer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/common_widgets/shimmer/shimmer_effect.dart';
+import 'package:spotify/core/common_widgets/shimmer/songs_collection_shimmer.dart';
+
+class PopularPlaylistsShimmer extends StatelessWidget {
+  const PopularPlaylistsShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 16,
+        mainAxisExtent: 200,
+      ),
+      delegate: SliverChildBuilderDelegate(
+              (_, index) => const SongsCollectionShimmer(),
+          childCount: 6
+      ),
+
+    );
+  }
+}
