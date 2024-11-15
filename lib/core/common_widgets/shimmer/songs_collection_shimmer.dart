6668b7594b89e32f70f81cef8779b1296f0522b0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-15 14:54:21 +0200

    adding songs collection shimmer widget

---
+++ lib/core/common_widgets/shimmer/songs_collection_shimmer.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/common_widgets/shimmer/shimmer_effect.dart';
+
+class SongsCollectionShimmer extends StatelessWidget {
+  const SongsCollectionShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: ShimmerEffect(
+            width: MediaQuery.sizeOf(context).width*0.3989,
+            height: MediaQuery.sizeOf(context).height*0.1899,
+            radius: 30,
+          ),
+        ),
+    
+        const Padding(
+          padding: EdgeInsets.only(left: 20,top: 4),
+          child: ShimmerEffect(width: 120, height: 12),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 20,top: 4),
+          child: ShimmerEffect(width: 70, height: 12),
+        ),
+      ],
+    );
+  }
+}
