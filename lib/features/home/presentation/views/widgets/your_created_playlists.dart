504043b6ff50c79e9bb41980a66045b036913c3c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-15 14:51:42 +0200

    adding your created playlists widget

---
+++ lib/features/home/presentation/views/widgets/your_created_playlists.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/common_widgets/shimmer/play_lists_horizontally_shimmer.dart';
+import 'package:spotify/features/home/presentation/views/widgets/songs_collection_container.dart';
+import 'package:spotify/features/home/presentation/views_model/home_controller.dart';
+
+class YourCreatedPlaylists extends StatelessWidget {
+  const YourCreatedPlaylists({super.key,});
+  @override
+  Widget build(BuildContext context) {
+    final controller = HomeController.instance;
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height*0.228,
+      child: Obx(
+            ()=> controller.isYourCreatedPlaylistsLoading.value? const PlaylistsHorizontallyShimmer() :ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => SongsCollectionContainer(songsCollection: controller.yourCreatedPlaylists.elementAt(index)),
+            separatorBuilder: (_, __) => const SizedBox(width: 12,),
+            itemCount: controller.yourCreatedPlaylists.length
+        ),
+      ),
+      
+    );
+  }
+}
