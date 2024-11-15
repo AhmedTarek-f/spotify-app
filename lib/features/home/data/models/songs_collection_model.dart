be5ab08ea24878161ca55a7f24efe44fbf7b396f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-15 14:49:29 +0200

    updating songs collection model

--- lib/features/home/data/models/songs_collection_model.dart
+++ lib/features/home/data/models/songs_collection_model.dart
@@ -1,17 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class SongsCollectionModel {
 
   const SongsCollectionModel({
+    required this.id,
     required this.collectionImg,
     required this.collectionTitle,
+    this.createdBy,
   });
-
+  final String id;
   final String collectionImg;
   final String collectionTitle;
+  final String? createdBy;
+
+  static SongsCollectionModel empty() => const SongsCollectionModel(id: "", collectionImg: "", collectionTitle: "");
 
   Map<String,dynamic> toJson() {
     return {
-      "CollectionImg":collectionImg,
-      "CollectionTitle":collectionTitle,
+      "Image":collectionImg,
+      "Name":collectionTitle,
+      "CreatedBy":createdBy,
     };
   }
+
+  factory SongsCollectionModel.fromSnapshot(DocumentSnapshot<Map<String,dynamic>> document) {
+    if(document.data() != null){
+      final snapshot = document.data()!;
+      return SongsCollectionModel(
+          id: document.id,
+          collectionImg: snapshot["Image"] as String,
+          collectionTitle: snapshot["Name"] as String,
+          createdBy: snapshot["CreatedBy"] as String?,
+      );
+    }
+    else{
+      return empty();
+    }
+  }
+
 }
