818d8d94971579f55b84cbef214ed7ac192fd8b4
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-15 14:53:23 +0200

    adding playlists horizoontally shimmer widget

---
+++ lib/core/common_widgets/shimmer/play_lists_horizontally_shimmer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/common_widgets/shimmer/songs_collection_shimmer.dart';
+
+class PlaylistsHorizontallyShimmer extends StatelessWidget {
+  const PlaylistsHorizontallyShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height*0.218,
+      child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => const SongsCollectionShimmer(),
+          separatorBuilder: (_, __) => const SizedBox(width: 12,),
+          itemCount: 6
+      ),
+    );;
+  }
+}
