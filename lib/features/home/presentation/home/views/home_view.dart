6fe5603fbe23c0515901e2bdf8855683c7d51a80
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-25 18:07:07 +0200

    adding home drawer

--- lib/features/home/presentation/home/views/home_view.dart
+++ lib/features/home/presentation/home/views/home_view.dart
@@ -1,50 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:spotify/core/common_widgets/sportify_custom_app_bar.dart';
-import 'package:spotify/core/constants/spotify_images.dart';
-import 'package:spotify/features/authentication/data/repository/authentication_repository.dart';
-import 'package:spotify/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:get/get.dart';
+import 'package:spotify/features/home/presentation/home/views/widgets/home_app_bar.dart';
+import 'package:spotify/features/home/presentation/home/views/widgets/home_drawer.dart';
+import 'package:spotify/features/home/presentation/home/views/widgets/home_view_body.dart';
+import 'package:spotify/features/home/presentation/home/views_model/home_controller.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final AuthenticationRepository authenticationController = AuthenticationRepository.instance;
-    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    return Scaffold(
-      appBar: SpotifyCustomAppBar(
-        isStartingWidgetEnabled: true,
-        startingWidget: InkWell(onTap: (){}, borderRadius: BorderRadius.circular(12), child: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: SvgPicture.asset(SpotifyImages.searchIcon,colorFilter: ColorFilter.mode(isDarkMode?const Color(0xffCFCFCF): const Color(0xff1C1B1B), BlendMode.srcIn),),
-        )),
-        isMiddleWidgetEnabled: true,
-        middleWidget: SvgPicture.asset(SpotifyImages.spotifyLogoSVG,height: MediaQuery.sizeOf(context).height*0.03909,),
-        isActionEnabled: true,
-        actionWidget: PopupMenuButton<String>(
-          onSelected: (value) async{
-            if (value == 'logout'){
-              await authenticationController.logout();
-            }
-          },
-          itemBuilder: (context) => [
-            const PopupMenuItem(
-              value: 'logout',
-              child: Row(
-                children: [
-                  Icon(Icons.logout, color: Colors.red),
-                  SizedBox(width: 8),
-                  Text('logout'),
-                ],
-              ),
-            ),
-          ],
-          icon:  SvgPicture.asset(SpotifyImages.threeDotsIcon),
-        ),
-      ),
-      body: const HomeViewBody(),
+    Get.put(HomeController());
+    return const Scaffold(
+      appBar: HomeAppBar(),
+      endDrawer: HomeDrawer(),
+      body:  HomeViewBody()
     );
   }
 }
+
 
---
+++ lib/features/home/presentation/home/views/widgets/home_drawer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/features/home/presentation/home/views/widgets/following_followers_drawer_section.dart';
+import 'package:spotify/features/home/presentation/home/views/widgets/logout_and_delete_acc_drawer_section.dart';
+import 'package:spotify/features/home/presentation/home/views/widgets/theme_drawer_section.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: MediaQuery.sizeOf(context).height*0.1),
+      child: Drawer(
+        width: MediaQuery.sizeOf(context).width*0.6,
+        child: Padding(
+          padding: EdgeInsets.only(left: MediaQuery.sizeOf(context).width*0.04, bottom: MediaQuery.sizeOf(context).height*0.03),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: MediaQuery.sizeOf(context).height*0.1,),
+              const FollowingFollowersDrawerSection(),
+              const SizedBox(height: 32,),
+              const ThemeDrawerSection(),
+              Expanded(child: Padding(
+                padding: EdgeInsets.only(right: MediaQuery.sizeOf(context).width*0.04),
+                child: const Divider(),
+              )),
+              const LogoutAndDeleteAccDrawerSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
