4c1d52bb2d7305dc6e329499e64dd5ef5843a6ad
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-25 18:47:45 +0200

    adding discovery follows and followers row

---
+++ lib/features/discovery/presentation/discovery_profile/views/widgets/discovery_follows_and_followers_row.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/constants/spotify_fonts.dart';
+import 'package:spotify/features/authentication/register/data/models/user_model.dart';
+
+class DiscoveryFollowsAndFollowersRow extends StatelessWidget {
+  const DiscoveryFollowsAndFollowersRow({super.key, required this.userData});
+  final UserModel userData;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+       Column(
+            children: [
+              Text((userData.following??0).toString(),style: SpotifyFonts.appStylesBold20),
+              const Text("Follows",style: SpotifyFonts.appStylesRegular14)
+            ],
+          ),
+
+        SizedBox(width: MediaQuery.sizeOf(context).width*0.3153,),
+         Column(
+            children: [
+              Text((userData.followers??0).toString(),style: SpotifyFonts.appStylesBold20),
+              const Text("Followers",style: SpotifyFonts.appStylesRegular14)
+            ],
+          ),
+
+      ],
+    );
+  }
+}
