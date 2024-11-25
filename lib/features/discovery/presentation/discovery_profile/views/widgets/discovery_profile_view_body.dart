c7ba6c3fb6e20bbc45b7763404768523e31cd7e6
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-25 18:36:24 +0200

    adding discovery profile view body

---
+++ lib/features/discovery/presentation/discovery_profile/views/widgets/discovery_profile_view_body.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/constants/spotify_images.dart';
+import 'package:spotify/features/authentication/register/data/models/user_model.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/views/widgets/discovery_public_songs_column.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/views/widgets/discovery_user_profile_details.dart';
+
+class DiscoveryProfileViewBody extends StatelessWidget {
+  const DiscoveryProfileViewBody({super.key, required this.userData});
+  final UserModel userData;
+  @override
+  Widget build(BuildContext context) {
+    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    return Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.sizeOf(context).width,
+                    decoration: BoxDecoration(
+                        color:isDarkMode? const Color(0xff2C2B2B) : Colors.white,
+                        borderRadius: const BorderRadius.only(bottomRight:Radius.circular(70) ,bottomLeft: Radius.circular(70))
+                    ),
+                    child:  DiscoveryUserProfileDetails(userData: userData,),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Image.asset(SpotifyImages.profileLeftSlashes,fit: BoxFit.scaleDown,),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 19,),
+              const DiscoveryPublicSongsColumn(),
+            ],
+          ),
+        ],
+      );
+  }
+}
