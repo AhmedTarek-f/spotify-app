cfd6daf9c31e63b8629ca48c2dea09c68651b940
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-25 18:46:30 +0200

    adding discovery public songs list view

---
+++ lib/features/discovery/presentation/discovery_profile/views/widgets/discovery_public_songs_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/common_widgets/shimmer/list_of_songs_shimmer.dart';
+import 'package:spotify/core/common_widgets/song_common_widgets/song_item.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/views_model/discovery_profile_controller.dart';
+
+class DiscoveryPublicSongsListView extends StatelessWidget {
+  const DiscoveryPublicSongsListView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final controller = DiscoveryProfileController.instance;
+    return Obx(
+        ()=> controller.isUserPublicSongsLoading.value? const ListOfSongsShimmer() :ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (_, index) => SongItem(songDetails: controller.publicSongsList.elementAt(index), isThreeDotsWidgetUsed: false, playlistSongs: controller.publicSongsList,index: index,),
+          separatorBuilder: (_, __) => const SizedBox(height: 12,),
+          itemCount: controller.publicSongsList.length
+      ),
+    );
+  }
+}
+
