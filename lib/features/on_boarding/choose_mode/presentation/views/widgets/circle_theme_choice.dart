5c38a5ebd213b61c9c1ed0ac5e7d568898a579dc
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-25 18:13:14 +0200

    adding light and dark theme switch functionality

--- lib/features/on_boarding/choose_mode/presentation/views/widgets/circle_theme_choice.dart
+++ lib/features/on_boarding/choose_mode/presentation/views/widgets/circle_theme_choice.dart
@@ -5,11 +5,13 @@ import 'package:spotify/core/constants/spotify_fonts.dart';
 class CircleThemeChoice extends StatelessWidget {
   const CircleThemeChoice({
     super.key,
-    required this.CircleBackgroundColor, required this.svgIcon, this.title, this.onTap,
+    required this.CircleBackgroundColor, required this.svgIcon, this.title, this.onTap, required this.svgColor, required this.titleColor,
   });
   final Color CircleBackgroundColor;
   final String svgIcon;
   final String? title;
+  final Color svgColor;
+  final Color titleColor;
   final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
@@ -20,11 +22,11 @@ class CircleThemeChoice extends StatelessWidget {
           child: CircleAvatar(
             backgroundColor: CircleBackgroundColor,
             radius: 36.5,
-            child: Center(child: SvgPicture.asset(svgIcon)),
+            child: Center(child: SvgPicture.asset(svgIcon,colorFilter: ColorFilter.mode(svgColor, BlendMode.srcIn),)),
           ),
         ),
         const SizedBox(height: 17,),
-        Text(title?? "",style: SpotifyFonts.appStylesMedium17.copyWith(color: const Color(0xffDADADA)),)
+        Text(title?? "",style: SpotifyFonts.appStylesMedium17.copyWith(color:titleColor),)
       ],
     );
   }
--- lib/features/on_boarding/choose_mode/presentation/views/widgets/light_or_dark_mode_choice.dart
+++ lib/features/on_boarding/choose_mode/presentation/views/widgets/light_or_dark_mode_choice.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:spotify/core/constants/spotify_images.dart';
 import 'package:spotify/features/on_boarding/choose_mode/presentation/views/widgets/circle_theme_choice.dart';
+import 'package:spotify/features/on_boarding/choose_mode/presentation/views_model/choose_mode_controller.dart';
 
 class LightOrDarkModeChoice extends StatelessWidget {
   const LightOrDarkModeChoice({
@@ -9,21 +11,38 @@ class LightOrDarkModeChoice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ChooseModeController());
     return Row(
       mainAxisAlignment:MainAxisAlignment.center,
       children: [
-        CircleThemeChoice(
-          CircleBackgroundColor:const Color(0xff3B3538),
-          svgIcon: SpotifyImages.moonIcon,
-          title: "Dark Mode",
-          onTap: (){},
+        Obx(
+          ()=> CircleThemeChoice(
+            CircleBackgroundColor:const Color(0xff3B3538),
+            svgIcon: SpotifyImages.moonIcon,
+            title: "Dark Mode",
+            svgColor: controller.isDarkMode.value?Colors.blue[900]!:Colors.white,
+            titleColor: controller.isDarkMode.value?Colors.blue[900]!:Colors.white,
+            onTap: ()async{
+              if(!controller.isDarkMode.value){
+                await controller.toggleThemeMode();
+              }
+            },
+          ),
         ),
         const SizedBox(width: 71,),
-        CircleThemeChoice(
-          CircleBackgroundColor:const Color(0xff31393C),
-          svgIcon: SpotifyImages.sunIcon,
-          title: "Light Mode",
-          onTap: (){},
+        Obx(
+          ()=> CircleThemeChoice(
+            CircleBackgroundColor:const Color(0xff31393C),
+            svgIcon: SpotifyImages.sunIcon,
+            title: "Light Mode",
+            svgColor: controller.isDarkMode.value?Colors.white:Colors.yellowAccent,
+            titleColor: controller.isDarkMode.value?Colors.white:Colors.yellowAccent,
+            onTap: ()async{
+              if(controller.isDarkMode.value){
+                await controller.toggleThemeMode();
+              }
+            },
+          ),
         ),
       ],
     );
