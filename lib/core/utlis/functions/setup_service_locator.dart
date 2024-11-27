9c79126e254764842ccca40134dfe06296f00849
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-27 19:46:32 +0200

    adding Image Picker for get it

--- lib/core/utlis/functions/setup_service_locator.dart
+++ lib/core/utlis/functions/setup_service_locator.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:just_audio/just_audio.dart';
 
 final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<GetStorage>(GetStorage());
   getIt.registerSingleton<AudioPlayer>(AudioPlayer());
+  getIt.registerSingleton<ImagePicker>(ImagePicker());
 }
