c4df9648a3835e433a6d0ca83bfc58aaaec1d6bb
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-27 20:00:02 +0200

    using home search song three dots

--- lib/features/home/presentation/home_search/views/widgets/songs_search_list.dart
+++ lib/features/home/presentation/home_search/views/widgets/songs_search_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:spotify/core/common_widgets/song_common_widgets/song_item.dart';
+import 'package:spotify/features/home/presentation/home_search/views/widgets/home_search_song_three_dots.dart';
 import 'package:spotify/features/home/presentation/home_search/views_model/home_search_controller.dart';
 import 'package:spotify/features/playlist_details/data/models/song_model.dart';
 
@@ -17,7 +18,7 @@ class SongsSearchList extends StatelessWidget {
             physics: const BouncingScrollPhysics(),
             itemBuilder: (_, index) {
               final List<SongModel> singleSong = [controller.songsSearchList[index]];
-              return SongItem(songDetails:controller.songsSearchList[index] ,index: index,playlistSongs:singleSong,isThreeDotsWidgetUsed: false,);
+              return SongItem(songDetails:controller.songsSearchList[index] ,index: index,playlistSongs:singleSong,isThreeDotsWidgetUsed: true, threeDotsWidget: HomeSearchSongThreeDots(song: controller.songsSearchList[index]),);
             },
             separatorBuilder: (_, __) => const SizedBox(height: 8,),
             itemCount: controller.songsSearchList.length
