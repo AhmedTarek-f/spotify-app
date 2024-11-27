6da897d361df2226a10b309593e8cb9679a69e74
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-27 19:55:05 +0200

    adding new functions createPlaylists & uploadPlaylistImage & addSongToCreatedPlaylists & addSongToRecentlyAndCreatedPlaylists

--- lib/features/home/data/repository/home_repository.dart
+++ lib/features/home/data/repository/home_repository.dart
@@ -1,12 +1,19 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 import 'package:spotify/features/home/data/data_sources/remote_data_sources/home_remote_data.dart';
 import 'package:spotify/features/home/data/models/new_album_model.dart';
 import 'package:spotify/features/home/data/models/songs_collection_model.dart';
 import 'package:spotify/features/playlist_details/data/models/song_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 class HomeRepository extends GetxController {
   static HomeRepository get instance => Get.find();
   final _homeRemoteData = Get.put(HomeRemoteData());
+  final SupabaseClient supaBase = Supabase.instance.client;
 
   Future<List<SongsCollectionModel>> fetchAllPlaylists() async{
     try{
@@ -58,6 +65,7 @@ class HomeRepository extends GetxController {
     }
   }
 
+
   Future<List<SongsCollectionModel>> fetchYourCreatedPlaylists() async {
     try {
       return await _homeRemoteData.fetchYourCreatedPlaylists();
@@ -72,6 +80,48 @@ class HomeRepository extends GetxController {
       return await _homeRemoteData.fetchSongs();
     }
     catch (e) {
+      throw e.toString();
+    }
+  }
+
+  Future<void> createPlaylists({required SongsCollectionModel playlist}) async{
+    try{
+      await _homeRemoteData.createPlaylists(playlist: playlist);
+    }
+    catch (e) {
+      throw e.toString();
+    }
+  }
+
+  Future<String> uploadPlaylistImage({required XFile image}) async {
+    try{
+      final String uniqueId = const Uuid().v4(); // Generate a unique identifier
+      final String path = "${uniqueId}_${basename(image.path)}";
+      final File imageFile = File(image.path);
+      await supaBase.storage.from("images/created_playlists").upload(path,imageFile);
+      return await supaBase.storage.from("images/created_playlists").getPublicUrl(path);
+    }
+    catch(e){
+      throw e.toString();
+    }
+  }
+
+  Future<void> addSongToCreatedPlaylists({required List<String>? listOfSongs, required String playlistId}) async{
+    try{
+      await _homeRemoteData.addSongToCreatedPlaylists(listOfSongs:listOfSongs,playlistId: playlistId);
+    }
+    catch (e)
+    {
+      throw e.toString();
+    }
+  }
+
+  Future<void> addSongToRecentlyAndCreatedPlaylists({required List<String>? listOfSongs, required String playlistId}) async{
+    try{
+      await _homeRemoteData.addSongToRecentlyAndCreatedPlaylists(listOfSongs:listOfSongs,playlistId: playlistId);
+    }
+    catch (e)
+    {
       throw e.toString();
     }
   }
