196618baa0897c5805e82119bb4dd8ca5f83f87b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-27 19:45:49 +0200

    accepting container title

--- lib/core/common_widgets/containers/updating_container/updating_container.dart
+++ lib/core/common_widgets/containers/updating_container/updating_container.dart
@@ -4,16 +4,17 @@ import 'package:spotify/core/constants/spotify_fonts.dart';
 
 class UpdatingContainer extends StatelessWidget {
   const UpdatingContainer({
-    super.key,
+    super.key, required this.containerTitle,this.width =200 ,this.height = 150,
   });
-
+  final String containerTitle;
+  final double width,height;
   @override
   Widget build(BuildContext context) {
     final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
     return Center(
         child: Container(
-            width: 200,
-            height: 150,
+            width: width,
+            height: height,
             decoration: BoxDecoration(
               color: isDarkMode? Colors.white :Colors.black87,
               borderRadius: BorderRadius.circular(12),
@@ -22,8 +23,8 @@ class UpdatingContainer extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Updating",style: SpotifyFonts.appStylesBold20.copyWith(color: isDarkMode? Colors.black : Colors.white),),
-                const SizedBox(height: 12,),
+                Text(containerTitle,style: SpotifyFonts.appStylesBold20.copyWith(color: isDarkMode? Colors.black : Colors.white),),
+                const SizedBox(height: 16,),
                 const CircularProgressIndicator(color: SpotifyColors.primaryColor,),
               ],
             )
