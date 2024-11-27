594f972a26132e601d0827855f7061e89735c8cf
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-11-27 19:44:01 +0200

    adding playlist songs three dots widget

---
+++ lib/features/playlist_details/views/widgets/playlist_songs_three_dots.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/constants/spotify_colors.dart';
+import 'package:spotify/features/playlist_details/data/models/song_model.dart';
+import 'package:spotify/features/playlist_details/views_model/playlist_details_controller.dart';
+
+class PlaylistSongsThreeDots extends StatelessWidget {
+  const PlaylistSongsThreeDots({super.key, required this.song,});
+  final SongModel song;
+  @override
+  Widget build(BuildContext context) {
+    final controller = PlaylistDetailsController.instance;
+
+    return IconButton(
+        splashColor: SpotifyColors.primaryColor.withOpacity(0.4),
+        highlightColor: SpotifyColors.primaryColor.withOpacity(0.4),
+        onPressed: () async {
+          await controller.deleteSongFromCreatedPlaylist(song: song);
+        },
+        icon: Obx(
+              ()=> (controller.isDeletingSongLoading.value && controller.deletedSongName == song.songTitle)?
+          const SizedBox(width: 16,height: 16,child: CircularProgressIndicator(color: Colors.redAccent,),)  :
+          const Icon(Icons.remove,color: Colors.redAccent,size: 32,),
+        )
+    );
+  }
+}
