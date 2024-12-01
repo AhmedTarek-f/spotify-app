8284e01c4b5691ace60a314111d8468ced65aba9
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-01 14:13:18 +0200

    adding following users list to the design

--- lib/features/discovery/presentation/discovery/views/widgets/discovery_view_body.dart
+++ lib/features/discovery/presentation/discovery/views/widgets/discovery_view_body.dart
@@ -4,6 +4,7 @@ import 'package:spotify/core/constants/spotify_images.dart';
 import 'package:spotify/core/utlis/loaders/animation_loader_widget.dart';
 import 'package:spotify/features/discovery/presentation/discovery/views/widgets/discovery_search_field.dart';
 import 'package:spotify/features/discovery/presentation/discovery/views/widgets/discovery_users_list.dart';
+import 'package:spotify/features/discovery/presentation/discovery/views/widgets/following_users_list.dart';
 import 'package:spotify/features/discovery/presentation/discovery/views_model/discovery_controller.dart';
 
 class DiscoveryViewBody extends StatelessWidget {
@@ -18,9 +19,13 @@ class DiscoveryViewBody extends StatelessWidget {
         children: [
           const SizedBox(height: 12,),
           const DiscoverySearchField(),
+          const SizedBox(height: 12,),
           Obx(
             ()=> controller.usersSearchList.isEmpty?
+            (controller.listOfUserPublicPlaylists.isEmpty?
             const Expanded(child: Center(child: AnimationLoaderWidget(text: "No Followers yet! follow some users so you can see their public playlists", animation: SpotifyImages.emptySearchAnimation))):
+            const Expanded(child: FollowingUsersList())
+            ):
             const Expanded(child: DiscoveryUsersList()),
           ),
         ],
