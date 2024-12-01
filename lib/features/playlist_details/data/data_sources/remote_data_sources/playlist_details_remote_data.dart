924478e4ce7520b12b777f209ab199858e87d37e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-01 14:22:20 +0200

    adding download song and image for offline mode

--- lib/features/playlist_details/data/data_sources/remote_data_sources/playlist_details_remote_data.dart
+++ lib/features/playlist_details/data/data_sources/remote_data_sources/playlist_details_remote_data.dart
@@ -1,17 +1,23 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:spotify/core/constants/api_keys.dart';
 import 'package:spotify/core/utlis/exceptions/t_firebase_exceptions.dart';
 import 'package:spotify/core/utlis/exceptions/t_format_exceptions.dart';
 import 'package:spotify/core/utlis/exceptions/t_platform_exceptions.dart';
 import 'package:spotify/features/home/data/models/songs_collection_model.dart';
 import 'package:spotify/features/playlist_details/data/models/song_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PlaylistDetailsRemoteData extends GetxController {
   static PlaylistDetailsRemoteData get instance => Get.find();
   final _db = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
+  final SupabaseClient supabase = Supabase.instance.client;
 
   Future<List<SongModel>> fetchPlaylistSongs({required List<String>? listOfSongs}) async {
     try{
@@ -77,6 +83,7 @@ class PlaylistDetailsRemoteData extends GetxController {
     try{
       await _db.collection("Users").doc(_auth.currentUser!.uid).collection("RecentlyPlayedPlaylists").doc("${playlist.collectionTitle}_${playlist.id}").update(
         {
+          "ListOfSongsIds": playlist.listOfSongsIds,
           "lastPlayedAt": DateTime.now().toIso8601String()
         },
       );
@@ -182,6 +189,40 @@ class PlaylistDetailsRemoteData extends GetxController {
     catch (e)
     {
       throw "Something went wrong: ${e.toString()}";
+    }
+  }
+
+  Future<String> downloadSong({required SongModel song}) async {
+    try {
+      // Download the file from Supabase Storage
+      final response = await supabase.storage.from(songsBucket).download("all_songs/${song.songBaseName}");
+
+      // Save the file locally
+      final directory = await getApplicationDocumentsDirectory();
+      final localFilePath = '${directory.path}/${song.songTitle}.mp3';
+      final file = File(localFilePath);
+      await file.writeAsBytes(response);
+
+      return localFilePath; // Return the local file path
+    } catch (e) {
+      throw "Error while downloading the song: ${e.toString()}";
+    }
+  }
+
+  Future<String> downloadSongImage({required SongModel song}) async {
+    try {
+      // Download the file from Supabase Storage
+      final response = await supabase.storage.from(imagesBucket).download("songs_images/${song.songBaseImage}");
+
+      // Save the file locally
+      final directory = await getApplicationDocumentsDirectory();
+      final localFilePath = '${directory.path}/${song.songBaseImage}';
+      final file = File(localFilePath);
+      await file.writeAsBytes(response);
+
+      return localFilePath; // Return the local file path
+    } catch (e) {
+      throw "Error while downloading the song image: ${e.toString()}";
     }
   }
 }
