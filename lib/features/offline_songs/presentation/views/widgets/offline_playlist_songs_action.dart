a5289a4fdc63a409a71d8b9dc120b49e06629760
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-01 13:59:54 +0200

    adding offline playlist songs action

---
+++ lib/features/offline_songs/presentation/views/widgets/offline_playlist_songs_action.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/constants/spotify_colors.dart';
+import 'package:spotify/features/offline_songs/presentation/views_model/offline_songs_controller.dart';
+import 'package:spotify/features/playlist_details/data/models/song_model.dart';
+
+class OfflinePlaylistSongsAction extends StatelessWidget {
+  const OfflinePlaylistSongsAction({super.key, required this.song,});
+  final SongModel song;
+  @override
+  Widget build(BuildContext context) {
+    final controller = OfflineSongsController.instance;
+
+    return InkWell(
+        splashColor: SpotifyColors.primaryColor.withOpacity(0.4),
+        highlightColor: SpotifyColors.primaryColor.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(50),
+        onTap: () async {
+          await controller.deleteSong(songId: song.songId);
+        },
+        child: const Icon(Icons.remove,color: Colors.redAccent,size: 32,),
+
+    );
+  }
+}
