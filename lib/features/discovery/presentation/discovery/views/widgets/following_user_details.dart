70d10ed1cbd9a4f36566161f5ffb666de0dc549b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-06 21:02:14 +0200

    making the user row can be pressed to view the user profile

--- lib/features/discovery/presentation/discovery/views/widgets/following_user_details.dart
+++ lib/features/discovery/presentation/discovery/views/widgets/following_user_details.dart
@@ -7,6 +7,7 @@ import 'package:spotify/core/constants/spotify_fonts.dart';
 import 'package:spotify/core/constants/spotify_images.dart';
 import 'package:spotify/features/discovery/data/model/user_details_with_created_playlists_model.dart';
 import 'package:spotify/features/discovery/presentation/discovery/views_model/discovery_controller.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/views/discovery_profile_view.dart';
 import 'package:spotify/features/home/presentation/home/views/widgets/expanded_songs_collection.dart';
 
 class FollowingUserDetails extends StatelessWidget {
@@ -34,41 +35,52 @@ class FollowingUserDetails extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16),
-              color: isDarkMode? const Color(0xff1B191A) : const Color(0xffF2F2F2)
+              // color: isDarkMode? const Color(0xff1B191A) : const Color(0xffF2F2F2)
           ),
           child: Column(
             children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30.5,
-                    backgroundColor: SpotifyColors.primaryColor,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.transparent,
-                      radius: 29.5,
-                      backgroundImage: ((userData.profileImg?.isEmpty??true) || userData.profileImg == null)?
-                      const AssetImage(SpotifyImages.profilePic):
-                      CachedNetworkImageProvider(userData.profileImg!),
+              InkWell(
+                splashColor: SpotifyColors.primaryColor.withOpacity(0.4),
+                highlightColor: SpotifyColors.primaryColor.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(16),
+                onTap: (){
+                  Get.to(
+                          ()=> DiscoveryProfileView(userData: userData),
+                      arguments: {"userId":userData.id}
+                  );
+                },
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30.5,
+                      backgroundColor: SpotifyColors.primaryColor,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 29.5,
+                        backgroundImage: ((userData.profileImg?.isEmpty??true) || userData.profileImg == null)?
+                        const AssetImage(SpotifyImages.profilePic):
+                        CachedNetworkImageProvider(userData.profileImg!),
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 12,),
-                  Text(userData.userName,style: SpotifyFonts.appStylesMedium17,)
-                ],
+                    const SizedBox(width: 12,),
+                    Text(userData.userName,style: SpotifyFonts.appStylesMedium17,)
+                  ],
+                ),
               ),
               const SizedBox(height: 12,),
               Obx(
                     ()=> controller.isListOfUserPublicPlaylistsLoading.value?
                 const PlaylistsHorizontallyShimmer() :
-                (userData.publicCreatedPlaylists.isEmpty?
+                (userData.publicCreatedPlaylists!.isEmpty?
                 Text("There is no available public playlist for ${userData.userName} yet.",style: SpotifyFonts.appStylesMedium14,overflow: TextOverflow.visible,textAlign: TextAlign.center,):
                 SizedBox(
                   height: MediaQuery.sizeOf(context).height*0.238,
                   child: ListView.separated(
                       physics: const BouncingScrollPhysics(),
                       scrollDirection: Axis.horizontal,
-                      itemBuilder: (_, index) => ExpandedSongsCollectionContainer(songsCollection: userData.publicCreatedPlaylists.elementAt(index)),
+                      itemBuilder: (_, index) => ExpandedSongsCollectionContainer(songsCollection: userData.publicCreatedPlaylists!.elementAt(index)),
                       separatorBuilder: (_, __) => const SizedBox(width: 8,),
-                      itemCount: userData.publicCreatedPlaylists.length
+                      itemCount: userData.publicCreatedPlaylists!.length
                   ),
                 )
                 ),
