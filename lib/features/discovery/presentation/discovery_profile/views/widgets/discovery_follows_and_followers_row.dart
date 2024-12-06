ae9e4e3b35f41224ec84b07e89b29e6cbf01d7bf
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-06 21:04:48 +0200

    updating the following and followers text part

--- lib/features/discovery/presentation/discovery_profile/views/widgets/discovery_follows_and_followers_row.dart
+++ lib/features/discovery/presentation/discovery_profile/views/widgets/discovery_follows_and_followers_row.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spotify/core/common_widgets/shimmer/shimmer_effect.dart';
 import 'package:spotify/core/constants/spotify_fonts.dart';
-import 'package:spotify/features/authentication/register/data/models/user_model.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/views_model/discovery_profile_controller.dart';
 
 class DiscoveryFollowsAndFollowersRow extends StatelessWidget {
-  const DiscoveryFollowsAndFollowersRow({super.key, required this.userData});
-  final UserModel userData;
+  const DiscoveryFollowsAndFollowersRow({super.key});
   @override
   Widget build(BuildContext context) {
+    final controller = DiscoveryProfileController.instance;
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
        Column(
             children: [
-              Text((userData.following??0).toString(),style: SpotifyFonts.appStylesBold20),
+              Obx(
+                    ()=>controller.isLoading.value?
+                const ShimmerEffect(width: 20, height: 12):
+                Text(
+                  (controller.followingFollowersData.value.following).toString(),
+                  style: SpotifyFonts.appStylesBold20,
+                ),
+              ),
               const Text("Following",style: SpotifyFonts.appStylesRegular14)
             ],
           ),
@@ -20,7 +29,14 @@ class DiscoveryFollowsAndFollowersRow extends StatelessWidget {
         SizedBox(width: MediaQuery.sizeOf(context).width*0.3153,),
          Column(
             children: [
-              Text((userData.followers??0).toString(),style: SpotifyFonts.appStylesBold20),
+              Obx(
+                    ()=>controller.isLoading.value?
+                const ShimmerEffect(width: 20, height: 12):
+                Text(
+                  (controller.followingFollowersData.value.followers).toString(),
+                  style: SpotifyFonts.appStylesBold20,
+                ),
+              ),
               const Text("Followers",style: SpotifyFonts.appStylesRegular14)
             ],
           ),
