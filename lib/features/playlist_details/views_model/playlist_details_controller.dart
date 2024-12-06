acb87688949b80fe74bd82bec27590b0e64b14ad
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-06 21:22:52 +0200

    updating playlist details controller logic

--- lib/features/playlist_details/views_model/playlist_details_controller.dart
+++ lib/features/playlist_details/views_model/playlist_details_controller.dart
@@ -1,5 +1,4 @@
 
-import 'dart:math';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
@@ -14,6 +13,7 @@ import 'package:spotify/features/offline_songs/data/models/offline_song_model.da
 import 'package:spotify/features/offline_songs/presentation/views_model/offline_songs_controller.dart';
 import 'package:spotify/features/playlist_details/data/repository/playlist_details_repository.dart';
 import 'package:spotify/features/playlist_details/data/models/song_model.dart';
+import 'package:spotify/features/song_details/presentation/views_model/song_details_controller.dart';
 
 class PlaylistDetailsController extends GetxController {
   static PlaylistDetailsController get instance => Get.find();
@@ -33,13 +33,19 @@ class PlaylistDetailsController extends GetxController {
   late final bool isCreatedPlaylistPublicOrNot;
   Rx<AudioPlayer> player = getIt.get<AudioPlayer>().obs;
   RxBool isShuffling = false.obs;
+  RxBool isRepeating = false.obs;
+  RxInt currentSongIndex = 0.obs;
+  Rx<Duration> position = Duration.zero.obs;
+  Rx<Duration> duration = Duration.zero.obs;
+  Rx<SongModel> currentSong = SongModel.empty().obs;
+  final bool isOfflineMode = Get.arguments["isOffline"] ?? false;
 
   @override
   void onInit() async{
     super.onInit();
     await _openHiveSongsBox();
-    addToRecentlyPlayedPlaylists(playlist: playlist);
-    UpdateRecentlyPlayedTime(playlist: playlist);
+    if(playlist.id != "001" && playlist.id != "002") addToRecentlyPlayedPlaylists(playlist: playlist);
+    if(playlist.id != "001" && playlist.id != "002" ) UpdateRecentlyPlayedTime(playlist: playlist);
     fetchPlaylistSongs(listOfSongs: playlist.listOfSongsIds);
     if((playlist.createdBy?.isNotEmpty ?? false) && playlist.createdBy != null) isCreatedPlaylistAtPublic(playlist: playlist);
   }
@@ -66,6 +72,7 @@ class PlaylistDetailsController extends GetxController {
       final allSongs = await _playlistDetailsRepository.fetchPlaylistSongs(listOfSongs: listOfSongs);
       if(allSongs.isNotEmpty){
         playlistSongs.assignAll(allSongs);
+        currentSong.value = playlistSongs.elementAt(0);
       }
       isSongsLoading.value = false;
     }
@@ -77,7 +84,9 @@ class PlaylistDetailsController extends GetxController {
 
   Future<void> UpdateRecentlyPlayedTime({required SongsCollectionModel playlist}) async {
     try{
-      await _playlistDetailsRepository.UpdateRecentlyPlayedTime(playlist: playlist);
+      if(homeController.recentlyPlayedPlaylists.any((playlistItem)=> (playlistItem.id == playlist.id) && (playlistItem.collectionTitle == playlist.collectionTitle))){
+        await _playlistDetailsRepository.UpdateRecentlyPlayedTime(playlist: playlist);
+      }
     }
     catch (e)
     {
@@ -220,46 +229,110 @@ class PlaylistDetailsController extends GetxController {
     }
   }
 
-  Future<void> toggleIsPlaying() async{
-    isPlaying.value = !isPlaying.value;
-    if(isPlaying.value) await fetchAllSongsOneByOne();
-    if(!isPlaying.value) await player.value.pause();
+  Future<void> toggleRepeat() async{
+    isRepeating.value = !isRepeating.value;
+    if(isRepeating.value)  await player.value.setLoopMode(LoopMode.one);
+    if(!isRepeating.value)  await player.value.setLoopMode(LoopMode.all);
   }
-
   Future<void> toggleShuffle() async{
     isShuffling.value = !isShuffling.value;
     if(isShuffling.value) await player.value.setShuffleModeEnabled(true);
     if(!isShuffling.value) await player.value.setShuffleModeEnabled(false);
   }
 
+  Future<void> toggleIsPlaying() async{
+    isPlaying.value = !isPlaying.value;
+    if(isPlaying.value) await fetchAllSongsOneByOne(songIndex: currentSongIndex.value);
+    if(!isPlaying.value) await player.value.pause();
+  }
 
-  Future<void> fetchAllSongsOneByOne() async {
-      final playlist = ConcatenatingAudioSource(
 
-        // Start loading next item just before reaching it
-        useLazyPreparation: true,
+  String formatDuration(Duration d){
+    final minutes = d.inMinutes.remainder(60);
+    final seconds = d.inSeconds.remainder(60);
+    return "${minutes.toString().padLeft(2,'0')}:${seconds.toString().padLeft(2,'0')}";
+  }
 
-        // Customise the shuffle algorithm
-        shuffleOrder: DefaultShuffleOrder(),
-
-        children: playlistSongs.map((song) => AudioSource.uri(Uri.parse(song.songUrl))).toList(),
-      );
-
-      // Load and play the playlist
-      await player.value.setAudioSource(
-        playlist,
-        initialIndex: player.value.currentIndex,
-        initialPosition: Duration.zero,
-      );
-
+  Future<void> handlePlayAndPause() async{
+    if(player.value.playing){
+      isPlaying.value=false;
+      await player.value.pause();
+    }
+    else{
+      isPlaying.value = true;
       await player.value.play();
+    }
+  }
+
+  void handleSeek(double value){
+    player.value.seek(Duration(seconds: value.toInt()));
+  }
+
+  Future<void> fetchAllSongsOneByOne({required int songIndex}) async {
+    currentSongIndex.value = songIndex;
+    final playlist = ConcatenatingAudioSource(
+      // Start loading next item just before reaching it
+      useLazyPreparation: true,
+      // Customise the shuffle algorithm
+      shuffleOrder: DefaultShuffleOrder(),
+      // Specify the playlist items
+      children: playlistSongs.map((song) => isOfflineMode?AudioSource.file(song.songUrl) :AudioSource.uri(Uri.parse(song.songUrl))).toList(),
+    );
+    // Load and play the playlist
+    await player.value.setAudioSource(playlist, initialIndex: currentSongIndex.value, initialPosition: Duration.zero);
+    player.value.positionStream.listen((p) {
+      position.value = p;
+    });
+    player.value.durationStream.listen((d) {
+      if(d != null){
+        duration.value = d;
+      }
+    });
+    player.value.currentIndexStream.listen((index) async{
+      if (index != null && playlistSongs.isNotEmpty) {
+        currentSongIndex.value = index;
+        currentSong.value = playlistSongs[index];
+        if(index >= (playlistSongs.length-1)){
+          await player.value.setLoopMode(LoopMode.all);
+        }
+      }
+    });
+    isPlaying.value = true;
+    await player.value.play();
+  }
+
+  Future<void> playPreviousSong() async {
+    if(player.value.currentIndex!>0){
+      await player.value.seekToPrevious();
+      currentSongIndex.value = player.value.currentIndex ??0;
+      currentSong.value = playlistSongs[currentSongIndex.value];
+      if( isOfflineMode == false && Get.isRegistered<SongDetailsController>()) await SongDetailsController.instance.fetchIfFavoriteSongs(songId: currentSong.value.songId);
+      if( isOfflineMode == false && Get.isRegistered<SongDetailsController>()) await SongDetailsController.instance.fetchIfPublicFavoriteSongs(songId: currentSong.value.songId);
+    }
+    else{
+      await fetchAllSongsOneByOne(songIndex: playlistSongs.length-1);
+    }
+  }
+
+  Future<void> playNextSong() async {
+    if(player.value.currentIndex! < playlistSongs.length-1){
+      await player.value.seekToNext();
+      currentSongIndex.value = player.value.currentIndex ??0;
+      currentSong.value = playlistSongs[currentSongIndex.value];
+      if( isOfflineMode == false && Get.isRegistered<SongDetailsController>()) await SongDetailsController.instance.fetchIfFavoriteSongs(songId: currentSong.value.songId);
+      if( isOfflineMode == false && Get.isRegistered<SongDetailsController>()) await SongDetailsController.instance.fetchIfPublicFavoriteSongs(songId: currentSong.value.songId);
+    }
+    else{
+      await fetchAllSongsOneByOne(songIndex: 0);
+    }
   }
 
   @override
-  void onClose() {
+  void onClose(){
+    if(isPlaying.value) homeController.songsListPlaying.assignAll(playlistSongs);
+    if(isPlaying.value) homeController.assignIndex();
     movingPlaylistToTop();
     if((playlist.createdBy?.isNotEmpty ?? false) && playlist.createdBy != null) checkIfAddOrDeletedFromPublic();
-    player.value.stop();
     super.onClose();
   }
 }
