aae86464bb789a29f728ae7be753eec6d89224ee
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-06 21:20:06 +0200

    adding the bottom navigation bar

--- lib/features/offline_songs/presentation/views/offline_songs_view.dart
+++ lib/features/offline_songs/presentation/views/offline_songs_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:spotify/features/offline_songs/presentation/views/widgets/offline_songs_bottom_nav.dart';
 import 'package:spotify/features/offline_songs/presentation/views/widgets/offline_songs_view_body.dart';
 import 'package:spotify/features/offline_songs/presentation/views_model/offline_songs_controller.dart';
 
@@ -7,9 +8,11 @@ class OfflineSongsView extends StatelessWidget {
   const OfflineSongsView({super.key,});
   @override
   Widget build(BuildContext context) {
-    Get.put(OfflineSongsController());
-    return  const Scaffold(
-      body: OfflineSongsViewBody(),
+    final controller = Get.put(OfflineSongsController());
+    return   Scaffold(
+      body: const OfflineSongsViewBody(),
+      bottomNavigationBar: Obx(()=> (!controller.isPlaying.value)? const SizedBox.shrink() : const OfflineSongsBottomNav() ),
     );
   }
+
 }
