1c9f6954e8cda5293bb09c0388e1cc907057eabe
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-06 21:05:56 +0200

    adding get following and followers data method

--- lib/features/discovery/presentation/discovery_profile/views_model/discovery_profile_controller.dart
+++ lib/features/discovery/presentation/discovery_profile/views_model/discovery_profile_controller.dart
@@ -1,17 +1,24 @@
 import 'package:get/get.dart';
+import 'package:spotify/core/utlis/loaders/loaders.dart';
 import 'package:spotify/features/discovery/data/repository/discovery_repository.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/data/models/following_followers_model.dart';
+import 'package:spotify/features/discovery/presentation/discovery_profile/data/repository/discovery_profile_repository.dart';
 import 'package:spotify/features/playlist_details/data/models/song_model.dart';
 
 class DiscoveryProfileController extends GetxController {
   static DiscoveryProfileController get instance => Get.find();
-  final _discoveryRepository = DiscoveryRepository.instance;
+  final _discoveryRepository = Get.isRegistered<DiscoveryRepository>()? DiscoveryRepository.instance : Get.put(DiscoveryRepository());
   final RxBool isUserPublicSongsLoading = false.obs;
+  final RxBool isLoading = false.obs;
+  final _discoveryProfileRepo = Get.put(DiscoveryProfileRepository());
+  final Rx<FollowingFollowersModel> followingFollowersData  = FollowingFollowersModel.empty().obs;
   RxList<SongModel> publicSongsList = <SongModel>[].obs;
   final String userId = Get.arguments["userId"];
 
   @override
   void onInit() {
     super.onInit();
+    getFollowingFollowersData(userId: userId);
     fetchUserPublicSongs(userId: userId);
   }
 
@@ -25,6 +32,18 @@ class DiscoveryProfileController extends GetxController {
     catch (e)
     {
       throw e.toString();
+    }
+  }
+
+  Future<void> getFollowingFollowersData({required String userId}) async{
+    try{
+      isLoading.value = true;
+      followingFollowersData.value = await _discoveryProfileRepo.getFollowingFollowersData(userId: userId);
+      isLoading.value=false;
+    }
+    catch(e) {
+      isLoading.value=false;
+      Loaders.errorSnackBar(title: "Oh Snap!",message: e.toString());
     }
   }
 }
