7ccb285ffe97bcca9b278ac6ea69e24ac33a112e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2024-12-06 20:50:08 +0200

    adjust the download button on pressed display and modified the song item logic

--- lib/core/common_widgets/song_common_widgets/song_item.dart
+++ lib/core/common_widgets/song_common_widgets/song_item.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:spotify/core/common_widgets/song_common_widgets/song_item_image.dart';
+import 'package:spotify/core/common_widgets/song_common_widgets/song_on_pressed_logic.dart';
 import 'package:spotify/core/constants/spotify_colors.dart';
 import 'package:spotify/core/constants/spotify_fonts.dart';
+import 'package:spotify/core/utlis/loaders/loaders.dart';
+import 'package:spotify/features/home/presentation/home/views_model/home_controller.dart';
 import 'package:spotify/features/playlist_details/data/models/song_model.dart';
 import 'package:spotify/features/playlist_details/views/widgets/download_song_button.dart';
 import 'package:spotify/features/playlist_details/views_model/playlist_details_controller.dart';
-import 'package:spotify/features/song_details/presentation/views/song_details_view.dart';
+import 'package:spotify/features/single_song_details/presentation/views/single_song_details_view.dart';
+
 
 class SongItem extends StatelessWidget {
   const SongItem({
@@ -17,7 +21,9 @@ class SongItem extends StatelessWidget {
     required this.playlistSongs,
     required this.index,
     this.isNetworkImage = true,
-    this.isOffline,
+    required this.isOffline,
+    this.isPlaying = false,
+    this.isSingleSong = false,
   });
   final SongModel songDetails;
   final Widget? threeDotsWidget;
@@ -25,29 +31,38 @@ class SongItem extends StatelessWidget {
   final List<SongModel> playlistSongs;
   final int index;
   final bool isNetworkImage;
-  final bool? isOffline;
+  final bool isOffline;
+  final bool isPlaying;
+  final bool isSingleSong;
   @override
   Widget build(BuildContext context) {
-    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
     return InkWell(
-      onTap: (){
-        Get.to(
-          () =>  const SongDetailsView(),
-          arguments: {"songDetails":songDetails,"playlistSongs": playlistSongs ,"index":index, "isOffline":isOffline},
-        );
+      onTap: () async{
+        if(isSingleSong){
+          FocusScope.of(context).unfocus();
+          HomeController.instance.songsListPlaying.clear();
+          Get.to(
+            () => const SingleSongDetailsView(),
+            arguments: {"songDetails":songDetails}
+          );
+        }
+        else{
+          await songOnPressedLogic(playlistSongs:playlistSongs ,index:index ,isOffline: isOffline,songDetails: songDetails);
+        }
       },
       splashColor: SpotifyColors.primaryColor.withOpacity(0.3),
       highlightColor: SpotifyColors.primaryColor.withOpacity(0.3),
       borderRadius: BorderRadius.circular(16),
       child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 3),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          border: Border.all(width: 1,color: SpotifyColors.primaryColor.withOpacity(0.5)),
           boxShadow: [
             BoxShadow(
-                color:isDarkMode? Colors.white54.withOpacity(0.1) : Colors.black38.withOpacity(0.1),
-              blurRadius: 5,
-              spreadRadius: 1
+                color:isPlaying? SpotifyColors.primaryColor :SpotifyColors.primaryColor.withOpacity(0.7),
+              blurRadius: isPlaying? 10:5,
+              spreadRadius: 1,
+              blurStyle: BlurStyle.outer
             ),
           ]
         ),
@@ -60,8 +75,8 @@ class SongItem extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(songDetails.songTitle,style: SpotifyFonts.appStylesBold16,maxLines: 1,),
-                  Text(songDetails.songAuthor,style: SpotifyFonts.appStylesRegular12,maxLines: 1,),
+                  Text(songDetails.songTitle,style: isPlaying? SpotifyFonts.appStylesBold16.copyWith(color: SpotifyColors.primaryColor.withOpacity(0.8)) :SpotifyFonts.appStylesBold16,maxLines: 1,),
+                  Text(songDetails.songAuthor,style: isPlaying? SpotifyFonts.appStylesRegular12.copyWith(color: SpotifyColors.primaryColor.withOpacity(0.8)): SpotifyFonts.appStylesRegular12,maxLines: 1,),
                 ],
               ),
             ),
@@ -70,13 +85,35 @@ class SongItem extends StatelessWidget {
               child: Row(
                   children: [
                     Expanded(child: Text(songDetails.songLength,style: SpotifyFonts.appStylesRegular15,)),
-                    if(Get.isRegistered<PlaylistDetailsController>()) Obx(()=>(PlaylistDetailsController.instance.isDownloadingProcess.value && PlaylistDetailsController.instance.downloadingSongId == songDetails.songId)? Padding(
+                    if(Get.isRegistered<PlaylistDetailsController>())Obx(
+                      ()=>(PlaylistDetailsController.instance.isDownloadingProcess.value && PlaylistDetailsController.instance.downloadingSongId == songDetails.songId)?
+                      Padding(
                       padding: const EdgeInsets.only(right: 8.0),
                       child: DownloadSongButton(song: songDetails,),
-                    ) :Expanded(
+                    ):
+                      PlaylistDetailsController.instance.isDownloadingProcess.value?
+                      InkWell(
+                        onTap: (){
+                          Loaders.warningSnackBar(title: "Note!",message: "There is a song in the downloading process, please wait for a few seconds ...");
+                        },
+                        child: AbsorbPointer(
+                          absorbing: true,
+                          child: Expanded(
+                            child: DownloadSongButton(song: songDetails,),
+                          ),
+                        ),
+                      )
+                      :Expanded(
                       child: DownloadSongButton(song: songDetails,),
-                    )) ,
-                    if(isThreeDotsWidgetUsed)Expanded(child: threeDotsWidget!),
+                    ),
+                    ) ,
+                    (isThreeDotsWidgetUsed && Get.isRegistered<PlaylistDetailsController>())?
+                    Obx(
+                      ()=>(Get.isRegistered<PlaylistDetailsController>() && PlaylistDetailsController.instance.isDeletingSongLoading.value && PlaylistDetailsController.instance.deletedSongName == songDetails.songTitle)?
+                      threeDotsWidget!:
+                      Expanded(child: threeDotsWidget!)
+                    ) :
+                   isThreeDotsWidgetUsed? Expanded(child: threeDotsWidget!) : const SizedBox.shrink(),
                   ],
                 ),
             )
